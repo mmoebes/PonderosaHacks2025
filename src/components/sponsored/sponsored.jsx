f46ac080631d808f9ffd6fb41ba3a99547a49765
mmoebes <maleea.grace@gmail.com>
2024-09-23 20:05:15 -0700

    add sponsor

--- src/components/sponsored/sponsored.jsx
+++ src/components/sponsored/sponsored.jsx
@@ -4,6 +4,12 @@ import SponserList from "./SponserList";
 import "./sponsored.css";
 import Pdf from "/images/SponsorInfo-v2.pdf";
 import { FaDownload } from "react-icons/fa";
+import S1 from "/images/Coconino.png";
+import S2 from "/images/Flagtag.png";
+import S3 from "/images/Hackclub.png";
+import S4 from "/images/NAU_logo.png";
+import S5 from "/images/Oreganos.png";
+import S6 from "/images/moonshot.png";
 
 const Sponsors = () => {
   return (
@@ -12,12 +18,41 @@ const Sponsors = () => {
       <h1 className="heading-tag-line1">Sponsors</h1>
       <hr className="heading-underline1" />
     </div>
-    <SponserList items={sponserData} />
+    <div className="responsive-multi-column-grid">
+       <div>
+               <a href="https://www.coconinofcu.org/" id="Coconino Federal Credit Union">
+                    <img src={S1} alt="Coconino Federal Credit Union" class="responsive1"/>
+                </a><br />
+        </div>
+        <div>
+               <a href="https://www.flagtagaz.com/" id="FlagTag">
+                    <img src={S2} alt="FlagTag" class="responsive1"/>
+                </a><br />
+        </div>
+        <div>
+               <a href="https://hackclub.com/" id="Hack Club">
+                    <img src={S3} alt="Hack Club" class="responsive1"/>
+                </a><br />
+        </div>
+        <div>
+               <a href="https://nau.edu/" id="Northern Arizona University">
+                    <img src={S4} alt="Northern Arizona University" class="responsive1"/>
+                </a><br />
+        </div>
+        <div>
+               <a href="https://oreganos.com/" id="Oregano's Pizza">
+                    <img src={S5} alt="Oregano's Pizza" class="responsive1"/>
+                </a><br />
+        </div>
+        <div>
+               <a href="https://www.moonshotaz.com/" id="Northern Arizona Technology & Business Incubator, Inc.">
+                    <img src={S6} alt="Northern Arizona Technology & Business Incubator, Inc." class="responsive1"/>
+                </a><br />
+        </div>
+    </div>
+
     <div className="sponsor-subsection">
     <div className="sponsor-left-section">
-        {/*<h2 className="sponsor-heading">
-        We need your help! 
-        </h2>*/}
         <h2 className="sponsor-heading"><br />
         This event is being organized by high school students - please support us!
         </h2>
