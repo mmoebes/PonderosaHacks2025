2145a822105e3f5f96856d33386e86cf66aa285a
mmoebes <maleea.grace@gmail.com>
2025-10-19 22:12:05 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/PonderosaHacks2025/' : '/',
-  plugins: [react()]
-}))
+export default defineConfig(({ mode }) => {
+  const base = mode === 'production' ? '/PonderosaHacks2025/' : '/'
+  return {
+    base,
+    plugins: [react()]
+  }
+})
